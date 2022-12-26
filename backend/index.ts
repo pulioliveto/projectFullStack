d5b97832a44737441313c70da460fa54bb81c17c
tatooliveto <tatooliveto10@gmail.com>
2022-12-26 11:05:02 -0300

    readme update

--- backend/index.ts
+++ backend/index.ts
@@ -10,7 +10,20 @@ const port: string | number = process.env.PORT || 8000;
 
 //Definicion de rutas
 app.get('/', (req: Request,res: Response) => {
-    res.json("Hola");
+    res.status(200).json({"data":{
+        "message": "goodbye world"
+    }});
 });
+
+app.get('/hello', (req: Request, res: Response) => {
+    const name = req.query.name;
+    if(!name) {
+        res.send(`Hola Anonimo`)
+    }else{
+res.send(`hola ${name}`)
+    }
+    
+
+})
 
 app.listen(port, ()=> console.log("Corriendo el servidor en", port))
